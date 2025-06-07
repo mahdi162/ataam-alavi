6b07a6e584892cc137a0990ae60263abeb7fd475
mahdi162 <adamzadehmahdi84@gmail.com>
2025-06-07 14:26:08 +0330

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+const users = [
+  { code: "ZT-12345", name: "علی رضایی", tokens: 3 },
+  { code: "ZT-67890", name: "مینا احمدی", tokens: 1 },
+  { code: "ZT-11223", name: "سارا مرادی", tokens: 2 }
+];
